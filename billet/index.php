a2a4add5e99714391e6f9a8a3f4f5e9bf800c418
Loïc Horellou <loic@formidable-studio.net>
2013-10-10 21:09:20 +0200

    update billet

--- billet/index.php
+++ billet/index.php
@@ -1,10 +1,10 @@
 <?php
-
-
 include('../classe/classe_billet.php');
 
 
 $billet = new billet('3220110088396');
 
+// echo $billet->HTMLticket;
 
-echo $billet->HTMLticket;
+// echo $billet->PDFurl;
+
