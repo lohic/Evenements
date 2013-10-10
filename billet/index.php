0685d3a963afabab5440727d05d4d2659cdb8853
Loïc Horellou <loic@formidable-studio.net>
2013-10-10 20:55:18 +0200

    Update index.php

--- billet/index.php
+++ billet/index.php
@@ -1,12 +1,8 @@
 <?php
 include('../classe/classe_billet.php');
 
-<<<<<<< HEAD
-$billet = new billet('1202101936871');
-=======
 
 $billet = new billet('1202101936871');
 
 
 echo $billet->PDFurl;
->>>>>>> 6e603b18bee61778d263ae80cd3192c1bd832694
