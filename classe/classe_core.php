d4fc21f636513443bc4b856eb06f4181d484d273
Loïc Horellou <loic@formidable-studio.net>
2014-02-05 16:17:01 +0100

    clean classe_core

--- classe/classe_core.php
+++ classe/classe_core.php
@@ -6,9 +6,7 @@ include_once(REAL_LOCAL_PATH.'classe/classe_fonctions.php');
 //include_once('fonctions.php');
 //include_once('classe_user.php');
 //include_once('connexion_vars.php');
-
 class Core {
-
 	var $isAdmin		= false;
 	var $evenement_db	= NULL;
 	var $news_db		= NULL;
@@ -18,7 +16,6 @@ class Core {
 	var $user_info		= NULL;
 	var $groups_id		= NULL;
 
-	
 	/* PREPARATION DU CONTENU DE LA PAGE */
 	function core(){
 		ini_set('arg_separator.output', '&amp;');
@@ -26,26 +23,21 @@ class Core {
 		date_default_timezone_set('UTC');
 
 		global $evenement_cInfo;
-		global $news_cInfo;
-		
+		global $news_cInfo;		
 		global $connexion_info;
 		
 		
-
 		$this->evenement_db = new connexion($connexion_info['server'],$connexion_info['user'],$connexion_info['password'],$connexion_info['db']);
 		//$this->news_db		= new connexion($news_cInfo['server'],$news_cInfo['user'],$news_cInfo['password'],$news_cInfo['db']);
-
-
 		//$this->user 		= new user($this->news_db);
 		$this->user 		= new spuser($this->evenement_db);
 
-
 		$this->user_info	= $this->user->get_user_info();
+
 		if(isset($this->user_info->groups)){
 			$this->groups_id	= array_keys($this->user_info->groups);
 		}
-		
-				
+					
 		if(isset($_POST['id_actual_group'])){
 			$_SESSION['id_actual_group'] = $_POST['id_actual_group'];
 		}else if(isset($_SESSION['id_actual_group'])){
